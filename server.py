928f2f993be6d3cfdce0adb1702090eb82a2ff4c
ThiagoStuginski <thiagostuginski@discente.ufg.br>
2026-03-23 22:12:19 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,39 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
+import struct
 
-s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
-s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client 
-while True:                # forever
-  data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
-conn.close()               # close the connection
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+print("Servidor aguardando operações...")
+
+while True:
+    (conn, addr) = s.accept() 
+    
+    data = conn.recv(17)#Recebe exatamente 17 bytes: 1 (char) + 8 (double) + 8 (double)
+    if not data or len(data) < 17: break
+
+    # Desempacota os dados: 'c' para char, 'd' para dois doubles
+    # O '!' no início indica "network byte order" (big-endian)
+    op_byte, n1, n2 = struct.unpack('!cdd', data)
+    op = op_byte.decode() # Converte b'+' para '+'
+
+    print(n1,op,n2)
+    
+    if op == '+':
+        res = n1 + n2
+    elif op == '-':
+        res = n1 - n2
+    elif op == '*':
+        res = n1 * n2
+    elif op == '/':
+        res = n1 / n2 if n2 != 0 else 0.0
+    else:
+        res = 0.0
+
+    # Retorna o resultado como um double binário (8 bytes)
+    conn.send(struct.pack('!d', res))
+    conn.close()
+
+
